388d6fec99b2db213f72cf27f704c107aacb503a
odf <olaf.delgado-friedrichs@anu.edu.au>
2015-12-22 14:41:42 +1100

    use NetCDF files for input and output

--- test-sedt.jl
+++ test-sedt.jl
@@ -1,14 +1,19 @@
 include("sedt.jl")
 
-A = reshape([
-             0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,
-             0,0,0,0,0, 0,1,1,1,0, 0,1,1,1,0, 0,1,1,1,0, 0,0,0,0,0,
-             0,0,0,0,0, 0,1,1,1,0, 0,1,1,1,0, 0,1,1,1,0, 0,0,0,0,0,
-             0,0,0,0,0, 0,1,1,1,0, 0,1,1,1,0, 0,1,1,1,0, 0,0,0,0,0,
-             0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0
-             ],
-            5, 5, 5)
-println(compute(A))
+using NetCDF
 
-A = reshape([0,0,0,0,0, 0,1,1,1,0, 0,1,1,1,0, 0,1,1,1,0, 0,0,0,0,0], 5, 5)
-println(compute(A))
+segmented = ncread(ARGS[1], "segmented")
+
+tomo = compute(segmented)
+
+(x, y, z) = size(tomo)
+filename  = "tomo_float_sedt.nc"
+varname   = "tomo_float"
+
+nccreate(filename, varname,
+         "$(varname)_xdim", x,
+         "$(varname)_ydim", y,
+         "$(varname)_zdim", z,
+         t = NC_FLOAT)
+
+ncwrite(tomo, filename, varname)
